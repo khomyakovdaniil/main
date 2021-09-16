152b77c3cbe69786ac88471edba88ae15ce634f4
khomyakovdaniil <khomyakovdaniil@gmail.com>
2021-09-16 17:01:49 +0300

    no switch

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,19 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
 
 func sort(numbers: [Int]) -> [Int]
 {
     var sorting = numbers
-    var sorted = false // FIXME: Implement without sorted (1.break+counter 2.break )
-    while !sorted {
-        sorted = true
+    var counter = 0 // FIXME: Implement without sorted (1.break+counter 2.break )
+    while counter < numbers.count {
         for i in 0 ..< (sorting.count-1) {
                 if sorting[i]>sorting[i+1]{
-                    sorted = false
                     var temp = sorting[i]
                     sorting[i] = sorting[i+1]
                     sorting[i+1] = temp
+                    counter = 0
+                }
+                else {
+                    counter+=1
                 }
         }
     }
@@ -21,7 +23,7 @@ func sort(numbers: [Int]) -> [Int]
 
 }
 
-var input = [1, 2, 3]
+var input = [11, 22, 5, 6, 8, 4, 1, 2, 3]
 
 var output = sort(numbers: input)
 print(output)
