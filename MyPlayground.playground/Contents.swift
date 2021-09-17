a3e483457441049aa953acdf7f545d655fa40b9f
khomyakovdaniil <khomyakovdaniil@gmail.com>
2021-09-17 14:52:25 +0300

    Update Contents.swift
    CodeStyle

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,33 +1,34 @@
 import UIKit
 
 
- 
-func sort(numbers: [Int]) -> [Int]
+
+func sortNumbers(numbers: [Int]) -> [Int]
 {
     var sorting = numbers
     var counter = 0 // FIXME: Implement without sorted (1.break+counter 2.break )
+    
     while true {
         for i in 0 ..< (sorting.count-1) {
-                if sorting[i]>sorting[i+1]{
-                    var temp = sorting[i]
-                    sorting[i] = sorting[i+1]
-                    sorting[i+1] = temp
-                }
-                else {
-                    counter+=1
-                }
-            if (i == numbers.count-2)&&(counter<numbers.count-1) {counter = 0}
+            if sorting[i] > sorting[i+1]{
+                var temp = sorting[i]
+                sorting[i] = sorting[i+1]
+                sorting[i+1] = temp
+            } else {
+                counter+=1
+            }
+            if (i == numbers.count-2)&&(counter<numbers.count-1) {
+                counter = 0
+            } //Sets counter to zero at the end of cycle
         }
-        if counter == numbers.count-1 {break}
+        if counter == numbers.count-1 {
+            break
+        } //Checks if all the numbers are sorted
     }
-print(counter)
-print(numbers.count)
-   return sorting
-
+    return sorting
 }
 
-var input = [17, 19, 13, 11, 22, 5, 6, 8, 4, 1, 2, 3, 40, 7, 162, 15]
+var input = [17, 19, 13, 11, 22, 5, 6, 8, 4, 1, 2, 3, 40, 7, 162, 15] //Sample input
+var output = sortNumbers(numbers: input)
 
-var output = sort(numbers: input)
 print(output)
 
